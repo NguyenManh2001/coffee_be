bc0c66c4679643c2391fa1303c50a8e8df4528c6
Nguyen Manh <manh95405@gmail.com>
2023-10-01 13:27:28 +0700

    update_Payment

--- api/controller/PaymentController.js
+++ api/controller/PaymentController.js
@@ -1,6 +1,7 @@
 let querystring = require("qs");
 let crypto = require("crypto");
 const moment = require("moment");
+const request = require("request");
 // const dateFormat = require("date-format");
 // const dateFormat = require("dateformat");
 class PaymentController {
@@ -39,7 +40,7 @@ class PaymentController {
     vnp_Params["vnp_OrderInfo"] = "Thanh toan cho ma GD:" + orderId;
     vnp_Params["vnp_OrderType"] = "other";
     vnp_Params["vnp_Amount"] = amount * 100;
-    vnp_Params["vnp_ReturnUrl"] = encodeURIComponent(returnUrl);
+    vnp_Params["vnp_ReturnUrl"] = returnUrl;
     vnp_Params["vnp_IpAddr"] = ipAddr;
     vnp_Params["vnp_CreateDate"] = createDate;
     if (bankCode !== null && bankCode !== "" && bankCode !== undefined) {
@@ -76,11 +77,11 @@ class PaymentController {
 
   async vnpayReturn(req, res, next) {
     let vnp_Params = req.query;
-
     let secureHash = vnp_Params["vnp_SecureHash"];
+    console.log(vnp_Params);
 
-    delete vnp_Params["vnp_SecureHash"];
-    delete vnp_Params["vnp_SecureHashType"];
+    // delete vnp_Params["vnp_SecureHash"];
+    // delete vnp_Params["vnp_SecureHashType"];
 
     vnp_Params = sortObject(vnp_Params);
     function sortObject(obj) {
@@ -187,6 +188,79 @@ class PaymentController {
     } else {
       res.status(200).json({ RspCode: "97", Message: "Checksum failed" });
     }
+  }
+
+  async queryDr(req, res, next) {
+    process.env.TZ = "Asia/Ho_Chi_Minh";
+    let date = new Date();
+
+    let vnp_TmnCode = process.env.YOUR_VNPAY_TMNCODE;
+    let secretKey = process.env.YOUR_VNPAY_HASH_SECRET;
+    let vnp_Api = process.env.vnp_Api;
+
+    let vnp_TxnRef = req.body.orderId;
+    let vnp_TransactionDate = req.body.transDate;
+
+    let vnp_RequestId = moment(date).format("HHmmss");
+    let vnp_Version = "2.1.0";
+    let vnp_Command = "querydr";
+    let vnp_OrderInfo = "Truy van GD ma:" + vnp_TxnRef;
+
+    let vnp_IpAddr =
+      req.headers["x-forwarded-for"] ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      req.connection.socket.remoteAddress;
+
+    let currCode = "VND";
+    let vnp_CreateDate = moment(date).format("YYYYMMDDHHmmss");
+
+    let data =
+      vnp_RequestId +
+      "|" +
+      vnp_Version +
+      "|" +
+      vnp_Command +
+      "|" +
+      vnp_TmnCode +
+      "|" +
+      vnp_TxnRef +
+      "|" +
+      vnp_TransactionDate +
+      "|" +
+      vnp_CreateDate +
+      "|" +
+      vnp_IpAddr +
+      "|" +
+      vnp_OrderInfo;
+
+    let hmac = crypto.createHmac("sha512", secretKey);
+    let vnp_SecureHash = hmac.update(new Buffer(data, "utf-8")).digest("hex");
+
+    let dataObj = {
+      vnp_RequestId: vnp_RequestId,
+      vnp_Version: vnp_Version,
+      vnp_Command: vnp_Command,
+      vnp_TmnCode: vnp_TmnCode,
+      vnp_TxnRef: vnp_TxnRef,
+      vnp_OrderInfo: vnp_OrderInfo,
+      vnp_TransactionDate: vnp_TransactionDate,
+      vnp_CreateDate: vnp_CreateDate,
+      vnp_IpAddr: vnp_IpAddr,
+      vnp_SecureHash: vnp_SecureHash,
+    };
+    // /merchant_webapi/api/transaction
+    request(
+      {
+        url: vnp_Api,
+        method: "POST",
+        json: true,
+        body: dataObj,
+      },
+      function (error, response, body) {
+        console.log(response);
+      }
+    );
   }
 }
 module.exports = new PaymentController();
--- api/routers/payment.js
+++ api/routers/payment.js
@@ -3,6 +3,7 @@ const {
   createPayment,
   vnpayReturn,
   vnpayIpn,
+  queryDr,
 } = require("../controller/PaymentController");
 
 const router = express.Router();
@@ -10,4 +11,5 @@ const router = express.Router();
 router.post("/create_payment", createPayment);
 router.get("/vnpay_return", vnpayReturn);
 router.get("/vnpay_ipn", vnpayIpn);
+router.post("/querydr", queryDr);
 module.exports = router;
