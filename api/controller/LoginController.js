75e0107daca3d9429d86c7474c4f9ccedcce94c6
Nguyen Manh <manh95405@gmail.com>
2023-11-10 12:39:10 +0700

    update

--- api/controller/LoginController.js
+++ api/controller/LoginController.js
@@ -4,54 +4,104 @@ const { default: mongoose } = require("mongoose");
 const bcrypt = require("bcrypt");
 class LoginController {
   //post
-  Login(req, res, next) {
-    AccountModel.find({ email: req.body.email })
-      .exec()
-      .then((user) => {
-        if (user.length < 1) {
-          return res.status(404).json({
-            message: "Auth faled",
-          });
-        } else {
-          bcrypt.compare(
-            req.body.password,
-            user[0].password,
-            function (err, result) {
-              if (err) {
-                return res.status(404).json({
-                  message: "Auth faled",
-                });
-              }
-              if (result) {
-                console.log(process.env.JWT_KEY);
-                const token = jwt.sign(
-                  {
-                    email: user[0].email,
-                    role: user[0].role,
-                    userId: user[0]._id,
-                  },
-                  process.env.JWT_KEY,
-                  {
-                    expiresIn: "1h",
-                  }
-                );
-                return res.status(200).json({
-                  message: "Đăng nhập thành công",
-                  token: token,
-                });
-              }
-              return res.status(404).json({
-                message: "Auth faled",
-              });
-            }
-          );
-        }
-      })
-      .catch((err) => {
-        res.status(500).json({
-          error: err,
+  // Login(req, res, next) {
+  //   AccountModel.find({ email: req.body.email })
+  //     .exec()
+  //     .then((user) => {
+  //       if (user.length < 1) {
+  //         return res.status(404).json({
+  //           message: "Auth faled",
+  //         });
+  //       } else {
+  //         bcrypt.compare(
+  //           req.body.password,
+  //           user[0].password,
+  //           function (err, result) {
+  //             if (err) {
+  //               return res.status(404).json({
+  //                 message: "Auth faled",
+  //               });
+  //             }
+  //             if (result) {
+  //               console.log(process.env.JWT_KEY);
+  //               const token = jwt.sign(
+  //                 {
+  //                   email: user[0].email,
+  //                   role: user[0].role,
+  //                   userId: user[0]._id,
+  //                 },
+  //                 process.env.JWT_KEY,
+  //                 {
+  //                   expiresIn: "1h",
+  //                 }
+  //               );
+  //               return res.status(200).json({
+  //                 message: "Đăng nhập thành công",
+  //                 token: token,
+  //               });
+  //             }
+  //             return res.status(404).json({
+  //               message: "Auth faled",
+  //             });
+  //           }
+  //         );
+  //       }
+  //     })
+  //     .catch((err) => {
+  //       res.status(500).json({
+  //         error: err,
+  //       });
+  //     });
+  // }
+
+  async Login(req, res, next) {
+    try {
+      const user = await AccountModel.findOne({ email: req.body.email }).exec();
+
+      if (!user) {
+        return res.status(404).json({
+          message: "Authentication failed",
         });
+      }
+
+      bcrypt.compare(
+        req.body.password,
+        user.password,
+        async function (err, result) {
+          if (err) {
+            throw new Error("Error comparing passwords");
+          }
+
+          if (result) {
+            const token = jwt.sign(
+              {
+                email: user.email,
+                role: user.role,
+                userId: user._id,
+              },
+              process.env.JWT_KEY,
+              {
+                expiresIn: "1h",
+              }
+            );
+
+            return res.status(200).json({
+              message: "Đăng nhập thành công",
+              token: token,
+            });
+          } else {
+            return res.status(404).json({
+              message: "Authentication failed",
+            });
+          }
+        }
+      );
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        error: "Internal Server Error",
       });
+    }
   }
 
   async listAcount(req, res, next) {
@@ -70,9 +120,9 @@ class LoginController {
       const data = await AccountModel.paginate(query, { page, limit });
       // const data = await CustomerModel.find({});
       if (data) {
-        res.send(data);
+        res.status(200).send(data);
       } else {
-        console.log("Không có dữ liệu");
+        res.status(404).send("Không có dữ liệu");
       }
     } catch (err) {
       console.log(err);
@@ -80,29 +130,51 @@ class LoginController {
     }
   }
 
+  // async updateAcount(req, res, next) {
+  //   const id = req.params.id;
+  //   AccountModel.updateOne({ _id: id }, { $set: req.body })
+  //     .exec()
+  //     .then((result) => {
+  //       res.status(200).json({
+  //         message: "Cập nhật tài khoản thành công",
+  //       });
+  //     })
+  //     .catch((err) => {
+  //       res.status(500).json({
+  //         error: err,
+  //       });
+  //     });
+  // }
+
   async updateAcount(req, res, next) {
-    const id = req.params.id;
-    AccountModel.updateOne({ _id: id }, { $set: req.body })
-      .exec()
-      .then((result) => {
-        res.status(200).json({
+    try {
+      const id = req.params.id;
+      const update = await AccountModel.updateOne(
+        { _id: id },
+        { $set: req.body }
+      ).exec();
+      if (update) {
+        return res.status(200).json({
           message: "Cập nhật tài khoản thành công",
         });
-      })
-      .catch((err) => {
-        res.status(500).json({
-          error: err,
+      } else {
+        return res.status(404).json({
+          message: "Không tìm thấy tài khoản để cập nhật",
         });
+      }
+    } catch (err) {
+      res.status(500).json({
+        error: err,
       });
+    }
   }
-
   async deleteAccount(req, res, next) {
     try {
       const data = await AccountModel.deleteOne({ _id: req.params.id });
       if (data) {
-        res.json("Xóa tài khoản thành công");
+        return res.status(200).json("Xóa tài khoản thành công");
       } else {
-        console.log("Không có dữ liệu");
+        return res.status(404).json("Không tìm thấy tài khoản để xóa");
       }
     } catch (err) {
       console.log(err);
